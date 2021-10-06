3a419de2258a5fe8e74aef6aaf805ce683e51a36
Arrththi Thavabalanathan <thavabaa@sheridancollege.ca>
2021-10-06 18:29:54 -0400

    commit parttimestudent

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,14 @@
+package student;
+
+public class PartTimeStudent extends Student {
+    private int numOfCourses;
+
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+}
