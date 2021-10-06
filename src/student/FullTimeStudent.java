7d9506ede41ec784d8f48830c989cc30fce86257
Arrththi Thavabalanathan <thavabaa@sheridancollege.ca>
2021-10-06 18:51:26 -0400

    adding new class FullTimeStudent

---
+++ src/student/FullTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * Arrththi Thavabalanathan
+ * 991528723
+ * SYST10199 - WEB PROGRAMMING
+ */
+package student;
+
+/**
+ *
+ * @author unimp
+ */
+public class FullTimeStudent extends Student{
+    private int numSemesters;
+
+    public int getNumSemesters() {
+        return numSemesters;
+    }
+
+    public void setNumSemesters(int numSemesters) {
+        this.numSemesters = numSemesters;
+    }
+    
+    
+}
